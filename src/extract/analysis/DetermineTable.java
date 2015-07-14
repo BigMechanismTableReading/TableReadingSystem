c9446be95c342b2ffd0e71d7535fe28b86239958
unknown <samsloate1@gmail.com>
2015-07-14 17:46:23 -0400

    Started on the Determine Table Pipeline

--- src/extract/analysis/DetermineTable.java
+++ src/extract/analysis/DetermineTable.java
@@ -1,7 +1,28 @@
 package extract.analysis;
 
+import java.util.HashMap;
+
+import columncontents.ColumnContents;
+import columncontents.Protein;
+import extract.buffer.TableBuf;
+import extract.buffer.TableBuf.Column;
+
 public class DetermineTable {
-	public static void main(String[]args){
-		ParticipantB  b= new ParticipantB();
+	
+	private boolean assignB(TableBuf.Table table, ParticipantB partB, HashMap<ColumnContents, Column> labels){
+		boolean hasProt = false;
+		for(TableBuf.Column col : table.getColumnList()){
+			Protein p = partB.hasParticipantB(col);
+			if(p != null){
+				labels.put(p, col);
+				hasProt = true;
+			}
+		}
+		return hasProt;
+	}
+	public void determine(TableBuf.Table table){
+		ParticipantB  partB = new ParticipantB();
+		HashMap<ColumnContents,TableBuf.Column> labels = new HashMap<ColumnContents,TableBuf.Column>();
+		assignB(table,partB,labels);
 	}
 }
