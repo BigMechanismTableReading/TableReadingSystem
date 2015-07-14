4792e4de426ae0e38a400419ef39fc2530cb73e1
hsiaov <hsiaov@5L24KQ1.corp.leidos.com>
2015-07-14 17:06:32 -0400

    Changed Type classes to match column content classes.

--- src/extract/types/Phosphorylation.java
+++ src/extract/types/Phosphorylation.java
@@ -4,12 +4,10 @@ import columncontents.*;
 import extract.analysis.TableType.ColumnTypes;
 
 public class Phosphorylation extends Reaction{
-	private String[] phosphositeRegex = {"a","cell"};
-	private String[] foldRegex = {"b", "cell"};
 	private Phosphorylation instance = null;
 	public Phosphorylation(){
-		data.put(English.getInstance(), phosphositeRegex);
-		data.put(ColumnTypes.FOLD, foldRegex);
+		data.add(English.getInstance());
+		data.add(Fold.getInstance());
 		conjugations.add("phosphorylat");
 	}
 	
--- src/extract/types/Reaction.java
+++ src/extract/types/Reaction.java
@@ -2,32 +2,19 @@ package extract.types;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import columncontents.ColumnContents;
 import extract.analysis.TableType;
 
 public abstract class Reaction {
-	HashMap<ColumnContents, String[]> data = new HashMap<ColumnContents, String[]>();
+	HashSet<ColumnContents> data = new HashSet<ColumnContents>();
 	ArrayList<String> conjugations = new ArrayList<String>();
 	public List<ColumnContents> getRequiredColumns() {
 		ArrayList<ColumnContents> requiredTypes = new ArrayList<ColumnContents>();
-		requiredTypes.addAll(data.keySet());
+		requiredTypes.addAll(data);
 		return requiredTypes;
-	}
-	public String getHeaderRegex(TableType.ColumnTypes type) {
-		if(data.containsKey(type)){
-			return data.get(type)[0];
-		} else {
-			return null;
-		}
-	}
-	public String getCellRegex(TableType.ColumnTypes type) {
-		if(data.containsKey(type)){
-			return data.get(type)[1];
-		} else {
-			return null;
-		}
 	}
 	public List<String> getConjugationBase(){
 		return conjugations;
