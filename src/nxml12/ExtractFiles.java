c66101727176aa6309ce5cc829b3e20f91a585c5
unknown <samsloate1@gmail.com>
2015-10-22 09:41:29 -0400

    ExtractFile uses the convertHTML method to convert the xml to an html file (works, output location is not decided)

--- src/nxml12/ExtractFiles.java
+++ src/nxml12/ExtractFiles.java
@@ -9,6 +9,16 @@ import java.util.Arrays;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.DocumentType;
@@ -20,22 +30,26 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class ExtractFiles {
-	
+
+
+	/**
+	 * Returns the text from the body and abstract tag of an nxml file.
+	 * @param fileName
+	 * @return
+	 */
 	public String getBodyText(String fileName){
 		File file = new File(fileName);
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db;
 		String body_text = "";
-		
 		try {
 			db = dbf.newDocumentBuilder();
-			Document doc = db.parse(file.getAbsoluteFile());
-			Element el = doc.getElementById("body");
-			body_text = el.getTextContent();
-//			NodeList n = el.getChildNodes();
-//			for(int i = 0; i < n.getLength(); i ++){
-//				n.item(0).getTextContent();
-//			}
+			Document doc = db.parse(file);
+			Element art = (Element) doc.getElementsByTagName("article");
+			Element body = (Element) art.getElementsByTagName("body");
+			Element abs = (Element) art.getElementsByTagName("abstract");
+			body_text = body.getTextContent()+abs.getTextContent();
+
 		} catch (SAXException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -48,4 +62,72 @@ public class ExtractFiles {
 		}
 		return body_text;
 	}
+
+	public File convertHTML(String fileName){
+		File xml = new File(fileName);
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		dbf.setValidating(true);
+		DocumentBuilder db;
+		try {
+			db = dbf.newDocumentBuilder();
+			Document doc = db.parse(xml);
+			Transformer t = TransformerFactory.newInstance().newTransformer();
+			t.setOutputProperty(OutputKeys.METHOD,"html");
+			Source source = new DOMSource(doc);
+			Result result = new StreamResult(fileName.replaceAll("nxml","html"));
+			t.transform(source, result);
+			
+			
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	/**
+	 * Return a NodeList of tables. Returns null if not found.
+	 * @param fileName
+	 * @return
+	 */
+	private NodeList getTables(String fileName){
+		File file = new File(fileName);
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db;
+		String []tables = null;
+		try{
+			db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file);
+			Element article = (Element)doc.getChildNodes().item(1);
+			NodeList table_elements = article.getElementsByTagName("table");
+			return table_elements;
+
+		}catch(SAXException e){
+
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null ;
+	}
 }
+
